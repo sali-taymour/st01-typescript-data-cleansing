7e0e799470d27630f5c4b0a89a27a7813787d650
sali <sali.taymor@gmail.com>
2022-08-18 11:26:06 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>TypeScript Data-Cleansing Example </h1>
+            <p>testing</p>
+        </div>
+    );
+}
+
+export default App;
